7ed1e7d7a58078f342a3bfdd97638a3c9e656931
skintang <tsjtux@gmail.com>
2017-09-10 14:43:20 -0500

    add 0803

--- src/c08RecursionDynProg/CC0803.java
+++ src/c08RecursionDynProg/CC0803.java
@@ -15,10 +15,40 @@ public class CC0803 {
         int mid = (start + end) / 2;
         if (arr[mid] == mid) {
             return mid;
-        } else if (arr[mid] > mid) {
-            return magicIndex(arr, start, mid - 1);
-        } else {
-            return magicIndex(arr, mid + 1, end);
         }
+        if (arr[mid] > mid) {
+            return magicIndex(arr, start, mid - 1);
+        }
+        return magicIndex(arr, mid + 1, end);
+    }
+
+    // follow up, duplicate numbers
+    public int magicIndexDup(int[] arr) {
+        return magicIndexDup(arr, 0, arr.length - 1);
+    }
+
+    private int magicIndexDup(int[] arr, int start, int end) {
+        if (start < end) {
+            return -1;
+        }
+
+        int mid = (start + end) / 2;
+
+        int midValue = arr[mid];
+        if (midValue == mid) {
+            return mid;
+        }
+        
+        // left
+        int leftIndex = Math.min(mid - 1, midValue);
+        int leftResult = magicIndexDup(arr, start, leftIndex);
+        if (leftResult > 0) {
+            return leftResult;
+        }
+
+        // right
+        int rightIndex = Math.max(mid + 1, midValue);
+        int rightResult = magicIndexDup(arr, rightIndex, end);
+        return rightResult;
     }
 }
