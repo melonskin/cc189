7b06b47a91e2c0147f412cdde0e213f1a69a224c
skintang <tsjtux@gmail.com>
2017-09-10 14:02:50 -0500

    add 0803 partially

--- src/c08RecursionDynProg/CC0803.java
+++ src/c08RecursionDynProg/CC0803.java
@@ -1,4 +1,24 @@
 package c08RecursionDynProg;
 
+/*
+ * Magic Index
+ *
+ */
 public class CC0803 {
+    public int magicIndex(int[] arr) {
+        return magicIndex(arr, 0, arr.length - 1);
+    }
+    private int magicIndex(int[] arr, int start, int end) {
+        if (end < start) {
+            return -1;
+        }
+        int mid = (start + end) / 2;
+        if (arr[mid] == mid) {
+            return mid;
+        } else if (arr[mid] > mid) {
+            return magicIndex(arr, start, mid - 1);
+        } else {
+            return magicIndex(arr, mid + 1, end);
+        }
+    }
 }
