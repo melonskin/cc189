4e8617e7acaa12b75a50ac9ab4faa2ef659cb326
skintang <tsjtux@gmail.com>
2017-09-11 17:17:18 -0500

    add 0810

--- src/c08RecursionDynProg/CC0810.java
+++ src/c08RecursionDynProg/CC0810.java
@@ -1,4 +1,27 @@
 package c08RecursionDynProg;
 
+/*
+ * Paint fill
+ */
 public class CC0810 {
+    enum Color {Black, White, Red}
+
+    public void paintFill(Color[][] screen, int r, int c, Color newColor) {
+        if (screen[r][c] == newColor) {
+            return;
+        }
+        paintFill(screen, r, c, screen[r][c], newColor);
+    }
+    private void paintFill(Color[][] screen, int r, int c, Color oldColor, Color newColor) {
+        if (r < 0 || r > screen.length - 1 || c < 0 || c > screen[0].length - 1) {
+            return;
+        }
+        if (screen[r][c] == oldColor) {
+            screen[r][c] = newColor;
+            paintFill(screen, r - 1, c, oldColor, newColor);
+            paintFill(screen, r + 1, c, oldColor, newColor);
+            paintFill(screen, r, c - 1, oldColor, newColor);
+            paintFill(screen, r, c + 1, oldColor, newColor);
+        }
+    }
 }
